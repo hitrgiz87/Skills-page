75d183ed0af1ae8ba50264be5e6cad1f301e12e5
hitrgiz87 <hitrgiz@gmail.com>
2023-11-06 21:07:49 -0600

    ufhjvhj

--- app/skills.js
+++ app/skills.js
@@ -1,18 +1,35 @@
 const ObservableArray = require("@nativescript/core/data/observable-array").ObservableArray;
 const frameModule = require("@nativescript/core/ui/frame");
 
-exports.pageLoaded = function(args) {
+exports.pageLoaded = async function(args) {
   const page = args.object;
   const listView = page.getViewById("myListView");
-console.log;
-  const itemList = new ObservableArray([
-    { name: 'Jim Bob', profilePicture: 'https://i.pravatar.cc/300', title: 'COO' },
-    { name: 'Suzy Test', profilePicture: 'https://i.pravatar.cc/300', title: 'CIO' },
-    // Add more items as needed...
-  ]);
 
-  listView.items = itemList;
+
+  let userData = await getUserData();
+
+  console.log(userData);
+
+  listView.items = userData;
 };
+
+async function getUserData() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/users');
+  let data = await response.json();
+  
+  data.forEach(user => {
+    let [firstName, lastName] = user.name.split(' ');
+    user.initials = `${firstName[0]}${lastName[0]}`;
+  });
+
+  let colors = ['background-color:blue', 'background-color:orange','background-color:green','background-color:red','background-color:yellow']
+
+  data.forEach(user => {
+    user.squareColor = colors[Math.floor(Math.random() * colors.length)];
+  });
+
+  return data;
+}
 
 exports.onItemTap = function(args) {
   const listView = args.object;
